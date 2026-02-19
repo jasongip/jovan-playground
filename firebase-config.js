131fbe09e22ae6f202834d4879714578c51b4984
jasongip <47200342+jasongip@users.noreply.github.com>
2026-02-19 10:56:56 +0000

    Update Firebase configuration with new project details

--- firebase-config.js
+++ firebase-config.js
@@ -3,12 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getFirestore, doc, setDoc, getDocs, collection, query, orderBy, limit } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyAO618ARy3iAmArgumicd98nlX_us7UcVk",
+  authDomain: "jovan-playground.firebaseapp.com",
+  projectId: "jovan-playground",
+  storageBucket: "jovan-playground.firebasestorage.app",
+  messagingSenderId: "429658166931",
+  appId: "1:429658166931:web:ee0c1c9a8568e4e13f5f66",
+  measurementId: "G-P77WCV8DMP"
 };
 
 // 初始化 Firebase
